01f77e98a74e461b65c516ffef56fb3c233179d9
Daniel Mares <danielmares32@gmail.com>
2021-05-20 17:40:19 -0500

    Se actualiza

--- consulta-medica/src/app/app.module.ts
+++ consulta-medica/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MedicoComponent } from './medico/medico.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MedicoComponent,
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
---
+++ consulta-medica/src/app/medico/medico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-medico',
+  templateUrl: './medico.component.html',
+  styleUrls: ['./medico.component.css']
+})
+export class MedicoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
