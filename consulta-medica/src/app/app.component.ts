358a07f1cf9a8a7bb83ff648e70d869749a07a35
krure <krurecaliomat@gmail.com>
2021-05-25 20:09:14 -0500

    act2

--- consulta-medica/src/app/app.component.ts
+++ consulta-medica/src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var name: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'consulta-medica';
   public isMenuCollapsed=true;
   public isFirst=true;
+
+  ngOnInit(): void {
+    new name();
+  }
 }
