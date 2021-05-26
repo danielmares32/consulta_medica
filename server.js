7dae6187d94cc3eb4a8036e9e6b7eff3a6779042
krure <krurecaliomat@gmail.com>
2021-05-25 21:01:14 -0500

    act5

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 let app = express();
 
-app.use(express.static(__dirname+'/consulta-medica/dist/consulta-medica'));
+app.use(express.static(__dirname+'/consulta-medica/src/index'));
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname+'/consulta-medica/src/index.html');
@@ -13,3 +13,23 @@ let server = app.listen("8081", "127.0.0.1" ,function(){
     let port = server.address().port;
     console.log("Example app listening at http://%s:%s", host, port);
 });
+
+//Conexion.js
+const mysql = require('mysql');
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'consulta_medica'
+});
+
+connection.connect();
+
+connection.query('SELECT * FROM paciente', function(err, rows, fields){
+    if(err)
+        console.error(err);
+    console.log('ID: '+rows[0].id+'\nNombre: '+rows[0].nombre);
+});
+
+connection.end();
+
