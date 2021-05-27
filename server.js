180a028df81588fb1e76b652ab72a6598ab2b6a3
Krure <Krurecaliomat@gmail.com>
2021-05-27 13:17:06 -0500

    Conex API 1

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const mysql = require('mysql');
 let connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'password',
     database: 'consulta_medica'
 });
 
